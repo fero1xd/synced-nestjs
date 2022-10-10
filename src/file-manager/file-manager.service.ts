53a3d32724f94806d0dfb3a8a5e6b1575e923e76
FeroniK <pranjalbutola12@gmail.com>
2022-10-10 22:53:08 +0530

    Using streams to write data

--- src/file-manager/file-manager.service.ts
+++ src/file-manager/file-manager.service.ts
@@ -1,21 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { join } from 'path';
 import { GenerateFileParams } from 'src/utils/types';
 import { v4 as uuid } from 'uuid';
-import { writeFileSync } from 'fs';
+import { createWriteStream } from 'fs';
 
 @Injectable()
 export class FileManagerService {
-  async generateFile(params: GenerateFileParams) {
-    const { extension, code } = params;
+  generateFile(params: GenerateFileParams) {
+    return new Promise<string>((res, rej) => {
+      const { extension, code } = params;
 
-    const fileId = uuid();
-    const fileName = `${fileId}.${extension}`;
+      const fileId = uuid();
+      const fileName = `${fileId}.${extension}`;
 
-    const filePath = join(process.env.CODES_DIRECTORY, fileName);
+      const filePath = join(process.env.CODES_DIRECTORY, fileName);
 
-    await writeFileSync(filePath, code);
+      // await writeFileSync(filePath, code);
 
-    return filePath;
+      const stream = createWriteStream(filePath);
+
+      stream.write(code, () => res(filePath));
+
+      stream.on('error', () => {
+        throw new InternalServerErrorException('Writing failed');
+      });
+    });
   }
 }
